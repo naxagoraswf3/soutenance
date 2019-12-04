b038b02e085f6b608cd03bba62551928c584ba53
ValentinMUZYCZKA <valentin.muzyczka@gmail.com>
2019-12-04 14:35:55 +0100

    mais l'odrinateur vas finir contre le mur dans tres peu de temps
    en vrais je vais FF

--- Site/src/Form/CommandeCoatingType.php
+++ Site/src/Form/CommandeCoatingType.php
@@ -2,18 +2,120 @@
 
 namespace App\Form;
 
+<<<<<<< HEAD
+=======
+
+>>>>>>> parent of 38a66b7... presque la fin
 use App\Entity\Fonction;
-use Captcha\Bundle\CaptchaBundle\Form\Type\CaptchaType;
-use Captcha\Bundle\CaptchaBundle\Validator\Constraints\ValidCaptcha;
-use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use App\Entity\CommandeCoating;
 use Symfony\Component\Form\AbstractType;
-use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Validator\Constraints\IsTrue;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Captcha\Bundle\CaptchaBundle\Form\Type\CaptchaType;
 use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\Form\Extension\Core\Type\HiddenType;
-use Symfony\Component\Form\FormBuilderInterface;
-use Symfony\Component\OptionsResolver\OptionsResolver;
-use Symfony\Component\Validator\Constraints\IsTrue;
+use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
+use Captcha\Bundle\CaptchaBundle\Validator\Constraints\ValidCaptcha;
 
+
+
+
+class CommandeCoatingType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('nom')
+            ->add('prenom')
+            ->add('mail')
+            ->add('resine', ChoiceType::class, [
+                "choices" => $this->getChoices(),
+                "multiple" => false
+            ])
+            ->add('application')
+            ->add('formulation', ChoiceType::class, [
+                "choices" => $this->getChoices2(),
+                "expanded" => true,
+                "multiple" => false
+            ])
+            ->add('provenance', ChoiceType::class, [
+                "choices" => $this->getChoices3(),
+                "expanded" => true,
+                "multiple" => false
+            ])
+            ->add('quantite')
+            ->add('complement')
+            ->add('fonction', EntityType::class,[
+                "class" => Fonction::class,
+                "choice_label"=>"name",
+                "multiple" => true,
+                "expanded" => false,
+                "help" => "Cliquer pour commencer la sélection"
+            ])
+
+            ->add('autrefonction', HiddenType::class)
+            ->add("captchaCode", CaptchaType::class,[
+                'captchaConfig'=> 'ValidationForm',
+                'constraints' => [
+                    new ValidCaptcha([
+                        'message' => 'invalid captcha'
+                    ])
+                ]
+            ])
+            ->add('agreeTerms', CheckboxType::class, [
+                'mapped' => false,
+                'constraints' => [
+                    new IsTrue([
+                        'message' => 'You should agree to our terms.',
+                    ]),
+                ],
+            ])
+
+
+        ;
+            
+            
+            ;
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => CommandeCoating::class,
+        ]);
+    }
+
+    private function getChoices(){
+        $choices = CommandeCoating::RESINE;
+        $output = [];
+        foreach ($choices as $k => $v) {
+            $output[$v] =$k;
+        }
+
+        return $output;
+    }
+
+       private function getChoices2(){
+        $choices = CommandeCoating::FORMULATION;
+        $output = [];
+        foreach ($choices as $k => $v) {
+            $output[$v] =$k;
+        }
+
+        return $output;
+    }
+
+       private function getChoices3(){
+        $choices = CommandeCoating::PROVENANCE;
+        $output = [];
+        foreach ($choices as $k => $v) {
+            $output[$v] =$k;
+        }
+
+<<<<<<< HEAD
 class CommandeCoatingType extends AbstractType {
 	public function buildForm(FormBuilderInterface $builder, array $options) {
 		$builder
@@ -76,4 +178,8 @@ class CommandeCoatingType extends AbstractType {
 			'data_class' => CommandeCoating::class,
 		]);
 	}
+=======
+        return $output;
+    }
+>>>>>>> parent of 38a66b7... presque la fin
 }
