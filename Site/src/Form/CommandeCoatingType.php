d5d0291310752bbd6f1a880b80c38b1ab30ea606
ValentinMUZYCZKA <valentin.muzyczka@gmail.com>
2019-12-04 14:36:29 +0100

    apparament c'est bon la

--- Site/src/Form/CommandeCoatingType.php
+++ Site/src/Form/CommandeCoatingType.php
@@ -3,6 +3,10 @@
 namespace App\Form;
 
 <<<<<<< HEAD
+<<<<<<< HEAD
+=======
+
+>>>>>>> parent of 38a66b7... presque la fin
 =======
 
 >>>>>>> parent of 38a66b7... presque la fin
@@ -21,6 +25,7 @@ use Captcha\Bundle\CaptchaBundle\Validator\Constraints\ValidCaptcha;
 
 
 
+<<<<<<< HEAD
 
 class CommandeCoatingType extends AbstractType
 {
@@ -179,6 +184,104 @@ class CommandeCoatingType extends AbstractType {
 		]);
 	}
 =======
+        return $output;
+    }
+>>>>>>> parent of 38a66b7... presque la fin
+=======
+
+class CommandeCoatingType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('nom')
+            ->add('prenom')
+            ->add('mail')
+            ->add('resine', ChoiceType::class, [
+                "choices" => $this->getChoices(),
+                "multiple" => false
+            ])
+            ->add('application')
+            ->add('formulation', ChoiceType::class, [
+                "choices" => $this->getChoices2(),
+                "expanded" => true,
+                "multiple" => false
+            ])
+            ->add('provenance', ChoiceType::class, [
+                "choices" => $this->getChoices3(),
+                "expanded" => true,
+                "multiple" => false
+            ])
+            ->add('quantite')
+            ->add('complement')
+            ->add('fonction', EntityType::class,[
+                "class" => Fonction::class,
+                "choice_label"=>"name",
+                "multiple" => true,
+                "expanded" => false,
+                "help" => "Cliquer pour commencer la sélection"
+            ])
+
+            ->add('autrefonction', HiddenType::class)
+            ->add("captchaCode", CaptchaType::class,[
+                'captchaConfig'=> 'ValidationForm',
+                'constraints' => [
+                    new ValidCaptcha([
+                        'message' => 'invalid captcha'
+                    ])
+                ]
+            ])
+            ->add('agreeTerms', CheckboxType::class, [
+                'mapped' => false,
+                'constraints' => [
+                    new IsTrue([
+                        'message' => 'You should agree to our terms.',
+                    ]),
+                ],
+            ])
+
+
+        ;
+            
+            
+            ;
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => CommandeCoating::class,
+        ]);
+    }
+
+    private function getChoices(){
+        $choices = CommandeCoating::RESINE;
+        $output = [];
+        foreach ($choices as $k => $v) {
+            $output[$v] =$k;
+        }
+
+        return $output;
+    }
+
+       private function getChoices2(){
+        $choices = CommandeCoating::FORMULATION;
+        $output = [];
+        foreach ($choices as $k => $v) {
+            $output[$v] =$k;
+        }
+
+        return $output;
+    }
+
+       private function getChoices3(){
+        $choices = CommandeCoating::PROVENANCE;
+        $output = [];
+        foreach ($choices as $k => $v) {
+            $output[$v] =$k;
+        }
+
         return $output;
     }
 >>>>>>> parent of 38a66b7... presque la fin
